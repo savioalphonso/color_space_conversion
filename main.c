1e9f2c309a326577589cb9487b45802923f00e07
michael <michael.baart96@gmail.com>
2020-08-14 03:30:14 -0700

    Fix logic in downsample_ycbcr_v1() and fix write

--- main.c
+++ main.c
@@ -56,14 +56,14 @@ uint32 * read_tiff_image(char* filename){
 }
 
 void write_tiff_image(uint8 *image, char* filename, int width, int height, int cb_subsampling, int cr_subsampling) {
-   // printf("[+] Creating output file \033[1;36m%s\033[0m\n", filename);
+    // printf("[+] Creating output file \033[1;36m%s\033[0m\n", filename);
     char* f = calloc(100, sizeof(char));
     char* ext = ".tiff";
     strcat(f, filename);
     strcat(f, ext);
     TIFF* tiff_output = TIFFOpen(f, "w");
     int chroma_values = 2;
-    int n_samples = 3;
+    int n_samples = 6;
 
     if (!tiff_output){
         printf("[-] \033[0;31mCould not create %s\033[0m\n", filename);
@@ -73,22 +73,22 @@ void write_tiff_image(uint8 *image, char* filename, int width, int height, int c
     //printf("[o] Setting TIFF Tags\n");
     TIFFSetField(tiff_output, TIFFTAG_IMAGEWIDTH, width);
     TIFFSetField(tiff_output, TIFFTAG_IMAGELENGTH, height);
-    TIFFSetField(tiff_output, TIFFTAG_SAMPLESPERPIXEL, n_samples);
+    TIFFSetField(tiff_output, TIFFTAG_SAMPLESPERPIXEL, 3);
     TIFFSetField(tiff_output, TIFFTAG_BITSPERSAMPLE, 8);
     TIFFSetField(tiff_output, TIFFTAG_ORIENTATION, (int)ORIENTATION_BOTLEFT);
     TIFFSetField(tiff_output, TIFFTAG_PLANARCONFIG, PLANARCONFIG_CONTIG);
     TIFFSetField(tiff_output, TIFFTAG_COMPRESSION, COMPRESSION_NONE);
     TIFFSetField(tiff_output, TIFFTAG_PHOTOMETRIC, PHOTOMETRIC_YCBCR);
-    TIFFSetField(tiff_output, TIFFTAG_YCBCRSUBSAMPLING, cb_subsampling, cr_subsampling);
+    TIFFSetField(tiff_output, TIFFTAG_YCBCRSUBSAMPLING, 2, 2);
     TIFFSetField(tiff_output, TIFFTAG_YCBCRPOSITIONING, YCBCRPOSITION_COSITED);
     //printf("[+] \033[1;32mSuccessfully Set TIFF Tags\033[0m\n");
 
-    size_t bytes_per_line = ((chroma_values * width) * 1/cb_subsampling) + width; //adjust for subsampling
+    size_t bytes_per_line = TIFFScanlineSize(tiff_output); //adjust for subsampling
     uint8* buffer = malloc(bytes_per_line);
 
     //printf("[o] Writing TIFF Image\n");
     for (int row = 0; row < height; ++row) {
-        memcpy(buffer, &image[row * width * n_samples], width * n_samples);
+        memcpy(buffer, &image[row * bytes_per_line], bytes_per_line);
         if (TIFFWriteScanline(tiff_output, buffer, row, 0) < 0){
             printf("[-] \033[0;31mWriting data failed!\033[0m\n");
             exit(EXIT_FAILURE);
@@ -107,23 +107,25 @@ uint8* downsample_ycbcr(const uint8* ycbcr){
     uint8* downsampled_ycbcr = malloc(width * height * 3 / 2);
     uint32 row_size = width * 3;
 
-    for (int pixel = 0, downsampled_pixel = 0; pixel < width * height * 3; pixel+=6, downsampled_pixel+=6) {
+    int downsampled_pixel = 0;
+    for (int pixel = 0; pixel < (width * height * 3); pixel+=6) {
+        // if(end of row)
+        //      skip the next row since we have already processed its data
+        if(pixel != 0 && pixel % row_size == 0) {
+            pixel+=row_size;
+        }
+
         // store y00,y01,y10,y11,avg(cb00,cb01,cb10,cb11),avg(cr00,cr01,cr10,cr11)
         downsampled_ycbcr[downsampled_pixel] = ycbcr[pixel]; //y00
         downsampled_ycbcr[downsampled_pixel+1] = ycbcr[pixel+3]; //y01
         downsampled_ycbcr[downsampled_pixel+2] = ycbcr[pixel+row_size]; //y10
         downsampled_ycbcr[downsampled_pixel+3] = ycbcr[pixel+row_size+3]; //y11
-        uint32 cbSum = ycbcr[pixel+1] + ycbcr[pixel+4] + ycbcr[pixel+row_size+1] + ycbcr[pixel+row_size+4];
-        downsampled_ycbcr[downsampled_pixel+4] = cbSum/4; // avg(cb00,cb01,cb10,cb11)
-        uint32 crSum = ycbcr[pixel+2] + ycbcr[pixel+5] + ycbcr[pixel+row_size+2] + ycbcr[pixel+row_size+5];
-        downsampled_ycbcr[downsampled_pixel+5] = crSum/4; // avg(cr00,cr01,cr10,cr11)
+        int cbSum = ycbcr[pixel+1] + ycbcr[pixel+4] + ycbcr[pixel+row_size+1] + ycbcr[pixel+row_size+4];
+        downsampled_ycbcr[downsampled_pixel+4] = (uint8)(cbSum/4); // avg(cb00,cb01,cb10,cb11)
+        int crSum = ycbcr[pixel+2] + ycbcr[pixel+5] + ycbcr[pixel+row_size+2] + ycbcr[pixel+row_size+5];
+        downsampled_ycbcr[downsampled_pixel+5] = (uint8)(crSum/4); // avg(cr00,cr01,cr10,cr11)
 
-        // if(end of row)
-        //      skip a row
-        if(pixel != 0 && pixel % row_size == 0) {
-            pixel+=row_size-6;
-            downsampled_pixel-=6;
-        }
+        downsampled_pixel+=6;
         //printf("[o] Converting RGB to YCbCr: \033[1;36m%0.00f%%\033[0m \b\r", ((float) pixel/ (float) (width * height)) * 100);
         //DEBUG_PRINT("[+] Converted Pixel %d: [\033[1;37mY: %d \033[1;36mCb: %d \033[1;35mCr: %d\033[0m]\n", pixel, Y(ycbcr, pixel), Cb(ycbcr, pixel), Cr(ycbcr, pixel));
     }
@@ -135,35 +137,37 @@ uint8* downsample_ycbcr(const uint8* ycbcr){
 uint8* downsample_ycbcr_v1(const uint8* ycbcr){
     uint32 width = 640;
     uint32 height = 480;
+    uint32 row_size = width * 3;
     uint8* downsampled_ycbcr = malloc(width * height * 3 / 2);
     bool backfill = false;
 
+    int downsampled_pixel = 0;
     for (int pixel = 0; pixel < width * height * 3; pixel+=6) {
+        // if(start of next row)
+        //      change fill type
+        if(pixel != 0 && pixel % row_size == 0) {
+            backfill = !backfill;
+            downsampled_pixel -= backfill ? row_size : 0;
+        }
 
         if (!backfill){
             // if(first row to downsample)
             //      store y00,y01,_,_,sum(cb00,cb01),sum(cr00,cr01)
-            downsampled_ycbcr[pixel] = TIFFGetR(ycbcr[pixel]);
-            downsampled_ycbcr[pixel+1] = TIFFGetR(ycbcr[pixel+3]);
-            downsampled_ycbcr[pixel+4] = TIFFGetR(ycbcr[pixel+1]) + TIFFGetR(ycbcr[pixel+4]);
-            downsampled_ycbcr[pixel+5] = TIFFGetR(ycbcr[pixel+2]) + TIFFGetR(ycbcr[pixel+5]);
+            downsampled_ycbcr[downsampled_pixel] = ycbcr[pixel];
+            downsampled_ycbcr[downsampled_pixel+1] = ycbcr[pixel+3];
+            downsampled_ycbcr[downsampled_pixel+4] = (ycbcr[pixel+1] >> 2) + (ycbcr[pixel+4] >> 2);
+            downsampled_ycbcr[downsampled_pixel+5] = (ycbcr[pixel+2] >> 2) + (ycbcr[pixel+5] >> 2);
         }
         else if (backfill){
             // if(second row to downsample)
             //      store _,_,y10,y11,avg(cb00,cb01,cb10,cb11),avg(cr00,cr01,cr10,cr11)
-            downsampled_ycbcr[pixel+2] = TIFFGetR(ycbcr[pixel]);
-            downsampled_ycbcr[pixel+3] = TIFFGetR(ycbcr[pixel+3]);
-            downsampled_ycbcr[pixel+4] = (TIFFGetR(ycbcr[pixel+1]) + TIFFGetR(ycbcr[pixel+4]) + downsampled_ycbcr[pixel+4]) / 4;
-            downsampled_ycbcr[pixel+5] = (TIFFGetR(ycbcr[pixel+2]) + TIFFGetR(ycbcr[pixel+5]) + downsampled_ycbcr[pixel+5]) / 4;
+            downsampled_ycbcr[downsampled_pixel+2] = ycbcr[pixel];
+            downsampled_ycbcr[downsampled_pixel+3] = ycbcr[pixel+3];
+            downsampled_ycbcr[downsampled_pixel+4] = (ycbcr[pixel+1] >> 2) + (ycbcr[pixel+4] >> 2) + downsampled_ycbcr[downsampled_pixel+4];
+            downsampled_ycbcr[downsampled_pixel+5] = (ycbcr[pixel+2] >> 2) + (ycbcr[pixel+5] >> 2) + downsampled_ycbcr[downsampled_pixel+5];
         }
 
-        // if(end of row)
-        //      change fill type
-        if(pixel != 0 && pixel % (width * 3) == 0) {
-            backfill = !backfill;
-        }
-        //printf("[o] Converting RGB to YCbCr: \033[1;36m%0.00f%%\033[0m \b\r", ((float) pixel/ (float) (width * height)) * 100);
-        //DEBUG_PRINT("[+] Converted Pixel %d: [\033[1;37mY: %d \033[1;36mCb: %d \033[1;35mCr: %d\033[0m]\n", pixel, Y(ycbcr, pixel), Cb(ycbcr, pixel), Cr(ycbcr, pixel));
+        downsampled_pixel+=6;
     }
 
     return downsampled_ycbcr;
@@ -362,7 +366,7 @@ void measureDownsampling(uint8*(convert)(const uint32*), uint8*(downsample)(cons
     uint8* downsampled_ycbcr = downsample(ycbcr);
     gettimeofday(&stop, NULL);
     uint64 delta = (stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec;
-    printf("\033[1;36m[%s]\033[0m RGB TO YCbCr Conversion took \033[1;36m%lu\033[0m microseconds\n", tag, delta);
+    printf("\033[1;36m[%s]\033[0m Downsampling took \033[1;36m%lu\033[0m microseconds\n", tag, delta);
     write_tiff_image(downsampled_ycbcr, tag, 640, 480, 2, 2);
 
 }
@@ -381,8 +385,8 @@ int main(int argc, char* argv[]) {
 //    measure(convert_rgb_to_ycbcr_v2_5, rgb_image, "Shift Only");
 //    measure(convert_rgb_to_ycbcr_v3, rgb_image, "SIMD");
 
-    measureDownsampling(convert_rgb_to_ycbcr, downsample_ycbcr, rgb_image, "Downsample");
-//    measureDownsampling(convert_rgb_to_ycbcr, downsample_ycbcr_v1, rgb_image, "Downsample w/ Backfilling");
+    measureDownsampling(convert_rgb_to_ycbcr_v1, downsample_ycbcr, rgb_image, "Downsample");
+    measureDownsampling(convert_rgb_to_ycbcr_v1, downsample_ycbcr_v1, rgb_image, "Downsample with Backfilling");
 //    measure(downsample_ycbcr_v1(convert_rgb_to_ycbcr),rgb_image, "Downsample")
 
     return 0;
